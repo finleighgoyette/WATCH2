187eeb30252956cbd190f36b8e8974030430f5a8
finleigh <finleighgoyette@Finleighs-MacBook-Pro.local>
2019-04-10 14:22:58 -0400

    hg

--- review-js/js/main.js
+++ review-js/js/main.js
@@ -1,31 +1,5 @@
-var balance = 12345;
-
-balance = balance - 300;
-balance-= 300;  //same thing
-
-balance = balance + 1;
-balance += 1;
-balance++;
-
-var name = `George`
-//identical:
-console.log(name + "'s pet dog has" + balance + "dolloars");
-console.log(name + '\'s pet dog has $' + balance + 'dolloars');
-console.log(`${name}'s pet dog has $${balance} dolloars`);
-
-//boolean
-var happy = true;
-
-if (happy) {
-  console.log("Hello world!");
-}
-else {
-  console.log(":(")
-}
-
-if (balance > 10000 || balance < 1000000) {
-  console.log("baller!");
-}
-else if (balance >10000) {
-  console.log('baller!')
-}
+var button = document.getElementById("clickme"),
+  count = 0;
+button.onclick = function() {
+  count += 1
+};
