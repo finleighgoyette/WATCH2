04ae795e562819bb15b9a71792698a25a60c1daa
finleigh <finleighgoyette@Finleighs-MacBook-Pro.local>
2019-04-12 16:35:23 -0400

    gg

--- review-js/js/main.js
+++ review-js/js/main.js
@@ -1,5 +1,19 @@
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1
-};
+var button = document.getElementById("clickme");
+const results = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+}
+
+
+button.addEventListener('click', ({target}) => {
+  console.log('test');
+  if (!target.matches('button')) return;
+
+
+  // count += 1;
+  results[target.dataset.mood]++;
+  console.log(`You thought: ${target.dataset.mood}, the results are: `, results);
+});
